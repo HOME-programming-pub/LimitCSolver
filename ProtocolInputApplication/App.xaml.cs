769de5c17d089b1be320b87dc5a4a6cb1ceed08c
Sven Karol <svenkarol+github@gmail.com>
2024-08-30 16:29:25 +0200

    Fix app class namings

--- ProtocolInputApplication/App.xaml.cs
+++ ProtocolInputApplication/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace LimitCProtokollInput
+namespace LimitCSolver.ProtocolInputApplication
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- ProtocolInputApplication/View/MainWindow.xaml.cs
+++ ProtocolInputApplication/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LimitCProtokollInput
+namespace LimitCSolver.ProtocolInputApplication.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
